04301a9a19a0ae017dd1b4fd45b7c9e29e9532fd
Chintan Shah <chintan.a.shah24@gmail.com>
2016-08-20 22:27:31 +0530

    Refactored code to ensure one function performs one single task and added very naive input check

--- interfaceLiftBulkDownloader.py
+++ interfaceLiftBulkDownloader.py
@@ -4,56 +4,59 @@ import sys
 import urllib2
 import os
 
-def getList(url):
 
-	"""
+def get_list_of_image_urls(url):
+    """
 	This function takes a url as a parameter, extracts .jpg file names from href attribute and stores it in a list.
-	It then calls downloadFiles passing the list as a parameter.
+	It then returns that list
 
 	"""
 
-	url_to_use=url
-	header={'User-agent': 'Mozilla/5.0'}
-	page=requests.get(url_to_use,headers=header)
-	tree=html.fromstring(page.text)
-	n=tree.xpath('//*[@id="page"]/div[4]/div[1]/p[2]/b[2]/text()')
-	no_of_pages=int(n[0])
-	list_of_urls=tree.xpath('//*[@id="wallpaper"]/div/div/div[@class="preview"]/div[2]/div/a/@href')
-	
+    url_to_use = url
+    header = {'User-agent': 'Mozilla/5.0'}
+    page = requests.get(url_to_use, headers=header)
+    tree = html.fromstring(page.text)
+    n = tree.xpath('//*[@id="page"]/div[4]/div[1]/p[2]/b[2]/text()')
+    no_of_pages = int(n[0])
+    list_of_urls = tree.xpath('//*[@id="wallpaper"]/div/div/div[@class="preview"]/div[2]/div/a/@href')
 
-	for i in range(2,no_of_pages+1):
-		url_to_use=url_to_use+'index'+str(i)+'.html'
-		page=requests.get(url_to_use,headers=header)
-		url_to_use=url
-		tree=html.fromstring(page.text)
-		temp_list=tree.xpath('//*[@id="wallpaper"]/div/div/div[@class="preview"]/div[2]/div/a/@href')
-		list_of_urls=list_of_urls+temp_list
+    for i in range(2, no_of_pages + 1):
+        url_to_use = url_to_use + 'index' + str(i) + '.html'
+        page = requests.get(url_to_use, headers=header)
+        url_to_use = url
+        tree = html.fromstring(page.text)
+        temp_list = tree.xpath('//*[@id="wallpaper"]/div/div/div[@class="preview"]/div[2]/div/a/@href')
+        list_of_urls = list_of_urls + temp_list
 
-	print("Downloading files.....")
-	downloadFiles(list_of_urls)
+    return list_of_urls
 
-def downloadFiles(list_of_urls,path="imgs/"):
 
-	"""
+def download_images(list_of_urls, path="imgs/"):
+    """
 	This takes the list as a parameter and downloads them onto the harddisk into a imgs folder
 	"""
 
-	for i in range(0,len(list_of_urls)):
-		print("Downloading file "+str(i+1))
-		url='http://interfacelift.com'+list_of_urls[i]	
-		#this line checks if imgs directory doesn't exist, it creates one
-		if not os.path.exists(path):
-			os.makedirs(path)
-		filename=path+url.split("/")[-1]
-		opener=urllib2.build_opener()
-		opener.addheaders=[('User-agent','Mozilla/5.0')]
-		img=opener.open(url)
-		localFile=open(filename,'wb')
-		localFile.write(img.read())
-		localFile.close()
+    for url in list_of_urls:
+        print("Downloading file " + url)
+        absolute_url = 'http://interfacelift.com' + url
+        # this line checks if imgs directory doesn't exist, it creates one
+        if not os.path.exists(path):
+            os.makedirs(path)
+        filename = path + absolute_url.split("/")[-1]
+        opener = urllib2.build_opener()
+        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        img = opener.open(absolute_url)
+        localFile = open(filename, 'wb')
+        localFile.write(img.read())
+        localFile.close()
 
-	print("Done downloading.....")
+    print("Done downloading.....")
 
 
-url=str(sys.argv[1])
-getList(url)
+if __name__ == 'main':
+    url = str(sys.argv[1])
+    if len(url) > 0:
+        list_of_urls = get_list_of_image_urls(url)
+        download_images(list_of_urls)
+    else:
+        raise ValueError("Input URL required!")
